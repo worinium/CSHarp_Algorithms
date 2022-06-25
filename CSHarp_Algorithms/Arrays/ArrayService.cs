d0bf731ae58b030a9afef42f275e8be9d237e48b
Woriayibapri Hearty Alapher <alapherwori8g5@gmail.com>
2022-06-25 18:47:21 +0100

    Algorithms Implementations

---
+++ CSHarp_Algorithms/Arrays/ArrayService.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHarp_Algorithms.Arrays
+{
+    public class ArrayService
+    {
+        internal static void GetExercise1()
+        {
+            /*
+            1.Write a program in C# Sharp to store elements in an array and print it. Go to the editor
+            Test Data:
+                Input 10 elements in the array:
+                element - 0 : 1
+                element - 1 : 1
+                element - 2 : 2
+            */
+            int[] arr = new int[10];
+            int i;
+            Console.Write("\n\nRead and Print elements of an array:\n");
+            Console.Write("-----------------------------------------\n");
+
+            Console.Write("Input 10 elements in the array :\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nElements in array are: ");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+            }
+            Console.Write("\n");
+        }
+        internal static void GetExercise2()
+        {
+            /*
+            2. Write a program in C# Sharp to read n number of values in an array and display it in reverse order. Go to the editor
+                Test Data :
+                Input the number of elements to store in the array :3
+                Input 3 number of elements in the array :
+                element - 0 : 2
+                element - 1 : 5
+                element - 2 : 7
+            */
+            Console.Write("Input the number of elements to be stored in the array : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int[] arr = new int[n];
+            int i;
+
+            Console.Write($"Input {n} elements in the array :\n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            arr.ToList().ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+            Console.Write("\nElements in Revers form: ");
+            for (i = n - 1; i >= 0; i--)
+            {
+                Console.Write("{0}  ", arr[i]);
+            }
+            Console.Write("\n");
+        }
+
+
+        internal static void GetExercise3()
+        {
+            /*
+            3. Write a program in C# Sharp to find the sum of all elements of the array. Go to the editor
+                Test Data :
+                Input the number of elements to be stored in the array :3
+                Input 3 elements in the array :
+                element - 0 : 2
+                element - 1 : 5
+                element - 2 : 8
+                Expected Output :
+                Sum of all elements stored in the array is : 15
+            */
+            Console.Write("Input the number of elements to be stored in the array : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int[] arr = new int[n];
+            int i;
+
+            Console.Write($"Input {n} elements in the array :\n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Sum of all elements stored in the array is : " + arr.Sum());
+
+        }
+        internal static void GetExercise4()
+        {
+            /*
+            4. Write a program in C# Sharp to copy the elements one array into another array. Go to the editor
+            Test Data :
+            Input the number of elements to be stored in the array :3
+            Input 3 elements in the array :
+            element - 0 : 15
+            element - 1 : 10
+            element - 2 : 12
+            Expected Output:
+            The elements stored in the first array are :
+            15 10 12
+            The elements copied into the second array are :
+            15 10 12
+            */
+            Console.Write("Input the number of elements to be stored in the array : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int[] arr = new int[n], arr2 = new int[n];
+            int i;
+
+            Console.Write($"Input {n} elements in the array :\n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //  Array.Copy(arr, 0, arr2, 0, n);
+            arr.CopyTo(arr2, 0);
+
+            Console.WriteLine();
+            Console.Write("Original Array Elements: ");
+            arr.ToList().ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+            Console.Write("Copied Array Elements : ");
+            arr2.ToList().ForEach(x => Console.Write(x + " "));
+
+        }
+        internal static void GetExercise5()
+        {
+            /*
+            5. Write a program in C# Sharp to count a total number of duplicate elements in an array. Go to the editor
+            Test Data :
+            Input the number of elements to be stored in the array :3
+            Input 3 elements in the array :
+            element - 0 : 5
+            element - 1 : 1
+            element - 2 : 1
+            Expected Output :
+            Total number of duplicate elements found in the array is : 1
+            */
+            Console.Write("Input the number of elements to be stored in the array : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int[] arr = new int[n], arr2 = new int[n], arr3 = new int[n];
+            int i, j, mm = 1, ctr = 0;
+
+            Console.Write($"Input {n} elements in the array :\n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            arr.CopyTo(arr2, 0);
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (arr[i] == arr2[j])
+                    {
+                        arr3[j] = mm;
+                        mm++;
+                    }
+                }
+                mm = 1;
+            }
+            /*--------------- Prints the array ------------------------------------*/
+            for (i = 0; i < n; i++)
+            {
+                if (arr3[i] == 2)
+                {
+                    ctr++;
+                }
+            }
+            Console.Write("The number of duplicate elements is: {0} \n", ctr);
+            Console.Write("\n\n");
+        }
+        internal static void GetExercise6()
+        {
+            /*
+            6. Write a program in C# Sharp to print all unique elements in an array. Go to the editor
+            Test Data :
+            Input the number of elements to be stored in the array :3
+            Input 3 elements in the array :
+            element - 0 : 1
+            element - 1 : 5
+            element - 2 : 1
+            Expected Output :
+            The unique elements found in the array are :
+            5
+            */
+            int n, ctr = 0;
+            Console.Write("Input the number of elements to be stored in the array : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            int[] arr1 = new int[n];
+            int i, j, k;
+
+            Console.Write("Input {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            /*Checking duplicate elements in the array */
+            Console.Write("\nThe unique elements found in the array are : \n");
+            for (i = 0; i < n; i++)
+            {
+                ctr = 0;
+                /*Check duplicate bifore the current position and increase counter by 1 if found.*/
+                for (j = 0; j < i - 1; j++)
+                {
+                    /*Increment the counter when the seaarch value is duplicate.*/
+                    if (arr1[i] == arr1[j])
+                    {
+                        ctr++;
+                    }
+                }
+                /*Check duplicate after the current position and increase counter by 1 if found.*/
+                for (k = i + 1; k < n; k++)
+                {
+                    /*Increment the counter when the seaarch value is duplicate.*/
+                    if (arr1[i] == arr1[k])
+                    {
+                        ctr++;
+                    }
+                    /* Duplicate numbers next to each other*/
+                    if (arr1[i] == arr1[i + 1])
+                    {
+                        i++;
+                    }
+                }
+                /*Print the value of the current position of the array as unique value when counter remain contains its initial value.*/
+                if (ctr == 0)
+                {
+                    Console.Write("{0} ", arr1[i]);
+                }
+            }
+            Console.Write("\n\n");
+
+        }
+
+
+        internal static void GetExercise7()
+        {
+            /*
+            7. Write a program in C# Sharp to merge two arrays of same size sorted in ascending order. Go to the editor
+                Test Data :
+                Input the number of elements to be stored in the first array :3
+                Input 3 elements in the array :
+                element - 0 : 1
+                element - 1 : 2
+                element - 2 : 3
+                Input the number of elements to be stored in the second array :3
+                Input 3 elements in the array:
+                element - 0 : 1
+                element - 1 : 2
+                element - 2 : 3
+                Expected Output:
+                The merged array in ascending order is :
+                1 1 2 2 3 3
+            */
+
+            throw new NotImplementedException();
+        }
+
+        internal static void GetExercise8()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void GetExercise9()
+        {
+            throw new NotImplementedException();
+        }
+        internal static void GetExercise10()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
---
+++ CSHarp_Algorithms/Program.cs
@@ -0,0 +1,160 @@
+﻿using CSHarp_Algorithms.Arrays;
+using System;
+
+namespace CSHarp_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteAction();
+
+        }
+
+        private static void ExecuteAction()
+        {
+            Console.WriteLine("Select The Algorithm to Perform");
+            Console.WriteLine("1 - Basic Algorithm");
+            Console.WriteLine("2 - Basic Arrays");
+            Console.WriteLine("3 - Basic Arrays");
+            Console.WriteLine("4 - Basic Arrays");
+            Console.WriteLine("5 - Basic Arrays");
+            Console.WriteLine("6 - Basic Arrays");
+            Console.WriteLine("7 - Basic Arrays");
+            Console.WriteLine("9 - Basic Arrays");
+            Console.WriteLine("10 - Basic Arrays");
+            int option = int.Parse(Console.ReadLine());
+            if (option is 1)
+            {//
+
+            }
+            if (option is 2)
+            {//
+                BasicArrayAction();
+            }
+        }
+
+        private static void BasicArrayAction()
+        {
+            Console.WriteLine("Select one from the following Basic Arrays Excercise Solutions");
+            Console.WriteLine("1. Basic Algorithm");
+            Console.WriteLine("2. Basic Arrays");
+            Console.WriteLine("3. Basic Arrays");
+            Console.WriteLine("4. Basic Arrays");
+            Console.WriteLine("5. Basic Arrays");
+            Console.WriteLine("6. Basic Arrays");
+            Console.WriteLine("7. Basic Arrays");
+            Console.WriteLine("9, Basic Arrays");
+            Console.WriteLine("1. Basic Arrays");
+            int caseOption = int.Parse(Console.ReadLine());
+            if (caseOption == 1)
+                ArrayService.GetExercise1();
+            if (caseOption == 2)
+                ArrayService.GetExercise2();
+            if (caseOption == 3)
+                ArrayService.GetExercise3();
+            if (caseOption == 4)
+                ArrayService.GetExercise4();
+            if (caseOption == 5)
+                ArrayService.GetExercise5();
+            if (caseOption == 6)
+                ArrayService.GetExercise6();
+            if (caseOption == 7)
+                ArrayService.GetExercise7();
+            if (caseOption == 8)
+                ArrayService.GetExercise8();
+            if (caseOption == 9)
+                ArrayService.GetExercise9();
+            if (caseOption == 10)
+                ArrayService.GetExercise10();
+            ContinueAgain("BasicArrayAction");
+        }
+        private static void BasicAlgorithm()
+        {
+            Console.WriteLine("Select one from the following Basic Algorithm Excercise Solutions");
+            Console.WriteLine("1. Basic Algorithm");
+            Console.WriteLine("2. Basic Arrays");
+            Console.WriteLine("3. Basic Arrays");
+            Console.WriteLine("4. Basic Arrays");
+            Console.WriteLine("5. Basic Arrays");
+            Console.WriteLine("6. Basic Arrays");
+            Console.WriteLine("7. Basic Arrays");
+            Console.WriteLine("9, Basic Arrays");
+            Console.WriteLine("1. Basic Arrays");
+
+            int caseOption = int.Parse(Console.ReadLine());
+            if (caseOption == 1)
+                ArrayService.GetExercise1();
+            if (caseOption == 2)
+                ArrayService.GetExercise2();
+            if (caseOption == 3)
+                ArrayService.GetExercise3();
+            if (caseOption == 4)
+                ArrayService.GetExercise4();
+            if (caseOption == 5)
+                ArrayService.GetExercise5();
+            if (caseOption == 6)
+                ArrayService.GetExercise6();
+            if (caseOption == 7)
+                ArrayService.GetExercise7();
+            if (caseOption == 8)
+                ArrayService.GetExercise8();
+            if (caseOption == 9)
+                ArrayService.GetExercise9();
+            if (caseOption == 10)
+                ArrayService.GetExercise10();
+            ContinueAgain("BasicAlgorithm");
+        }
+
+        static void ContinueAgain(string action = "")
+        {
+            Console.WriteLine("Do You want to perform another operation (Y/N)");
+            string option = Console.ReadLine();
+            if (string.IsNullOrEmpty(option) || option.ToLower() is "no" || option.ToLower() is "n")
+            {
+                Console.WriteLine("Wrong Input");
+                ContinueAgain();
+            }
+            if (option.ToLower() is "no" || option.ToLower() is "n")
+            {
+                Environment.Exit(0);
+            }
+            if (option.ToLower() is "yes" || option.ToLower() is "y")
+            {
+                if (action == "BasicAlgorithm")
+                {
+                    //BasicAlgorithm();
+                    Console.WriteLine("Choose Appropriate Option");
+                    Console.WriteLine("1. Go Back To Main Menu");
+                    Console.WriteLine("2. Basic Algorithm");
+                    int opt = int.Parse(Console.ReadLine());
+                    if (opt is 1)
+                    {
+                        ExecuteAction();
+                    }
+                    if (opt is 2)
+                    {
+                        BasicAlgorithm();
+                    }
+
+                }
+                if (action == "BasicArrayAction")
+                {
+                    //BasicArrayAction();
+                    Console.WriteLine("Choose Appropriate Option");
+                    Console.WriteLine("1. Go Back To Main Menu");
+                    Console.WriteLine("2. Basic Array Algorithm");
+                    int opt = int.Parse(Console.ReadLine());
+                    if (opt is 1)
+                    {
+                        ExecuteAction();
+                    }
+                    if (opt is 2)
+                    {
+                        BasicArrayAction();
+                    }
+                }
+            }
+        }
+    }
+}
